fff5ef7dcaa2e40758a2b750202d16edb79fa3f0
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2019-07-02 15:08:44 +0530

    script to download gloVe

---
+++ getGloVe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get GloVe vectors
+if hash wget 2>/dev/null; then
+  wget http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip
+else
+  curl -O http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip
+fi
+unzip glove.6B.zip
+rm glove.6B.zip
