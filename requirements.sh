726fa86e7a7a294caa63383b633a357b5ee2d3cd
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2019-07-04 12:13:07 +0530

    commands to download glove and pydot

--- requirements.sh
+++ requirements.sh
@@ -2,9 +2,29 @@
 
 set -e
 
+FILE=glove.6B/glove.6B.50d.txt
+if test -f "$FILE"
+then
+    exists=$true
+    read -p "50d GloVe exists. Redownload? [y/n]: " ans
+    if [ $ans == 'y' ] || [ $ans == 'Y' ]
+    then
+        exists=$false
+    fi
+else
+    exists=$false
+fi
+
+if ! $exists
+then
+    bash get-glove.sh
+fi
+
 eval "$(conda shell.bash hook)"
-read -p 'enter python3 conda environment name: ' env
+read -p "enter python3 conda environment name: " env
 
 conda activate $env
-conda install matplotlib numpy keras nltk scikit-learn pandas
+conda install numpy keras nltk scikit-learn pandas
+conda install matplotlib graphviz python-graphviz
+pip install pydot
 python -m nltk.downloader popular
